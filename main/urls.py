b176991da8d69cd2a43d7075c53a1b8fdaad22ca
15collinst <15collinst@clevedonschool.org.uk>
2023-08-10 15:14:46 +0100

    removed extra folder

---
+++ main/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("todos/", views.todos, name="Todos"),
+    path("create/", views.create, name="index"),
+    path("<int:id>", views.index, name="index"),
+]
